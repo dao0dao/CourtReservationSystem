f250be879cdaae9c8981d49fcc4edae960d24a8d
Demid <d_greshnikov@tlen.pl>
2021-12-19 19:41:06 +0100

    dodano kasjerów

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoachesComponent } from './coaches/coaches.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'coaches', component: CoachesComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+
+  constructor(private route: Router, private Router: ActivatedRoute) { }
+
+  isMenu: boolean = false;
+
+  toggleMenu() {
+    this.isMenu = !this.isMenu;
+  }
+
+  ngOnInit(): void {
+    console.log(this.Router.snapshot);
+    // this.route.navigate([this.route.url]);
+
+  }
+
+
 }

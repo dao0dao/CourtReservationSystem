32f731e0e89053a039e000a1a05b46b42247156e
Demid <d_greshnikov@tlen.pl>
2021-12-22 19:55:44 +0100

    dodano odświerzający Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RefreshGuard } from './refresh.guard';
 
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+  { path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivateChild: [RefreshGuard] },
 ];
 
 @NgModule({

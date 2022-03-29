df129adefbe99e3bd92ef41164a6c53da28c0e8f
Demid <d_greshnikov@tlen.pl>
2022-03-29 21:40:22 +0200

    dodano obsługę radioboxów

--- src/app/pages/timetable/timetable-modal/timetable-modal.component.ts
+++ src/app/pages/timetable/timetable-modal/timetable-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { Player } from '../../players/interfaces';
 
@@ -24,18 +24,78 @@ export class TimetableModalComponent implements OnInit {
   playerTwo: Player[] = [];
 
   form: FormGroup = new FormGroup({});
-
+  getFormField(name: string): AbstractControl | null {
+    return this.form.get(name);
+  }
+  isListOne: boolean = true;
+  isListTwo: boolean = true;
+  isPlayerChosen: boolean = false;
 
   ngOnInit(): void {
     this.playerOne = this.players;
     this.playerTwo = this.players;
-    this.form = this.fb.group({
-      date: [this.date, [Validators.required]]
-    });
+    this.setForm();
   }
 
   closeModal() {
     this.outputCloseModal.emit(true);
+  }
+
+  setForm() {
+    this.form = this.fb.group({
+      date: [this.date, [Validators.required]],
+      from: [this.date, [Validators.required]],
+      to: [this.date, [Validators.required]],
+      playerOne: [''],
+      playerTwo: [''],
+      guestOne: [''],
+      guestTwo: [''],
+      court: ['', [Validators.required]],
+      // obsługa wyboru
+      listOne: ['true'],
+      listTwo: ['true'],
+    });
+  }
+
+  handleChoseOne() {
+    if (this.getFormField('listOne')?.value == 'true') {
+      this.isListOne = true;
+    }
+    if (this.getFormField('listOne')?.value == 'false') {
+      this.isListOne = false;
+    }
+    this.handleIsPlayerChosen();
+  }
+
+  handleChoseTwo() {
+    if (this.getFormField('listTwo')?.value == 'true') {
+      this.isListTwo = true;
+    }
+    if (this.getFormField('listTwo')?.value == 'false') {
+      this.isListTwo = false;
+    }
+    this.handleIsPlayerChosen();
+  }
+
+  handleIsPlayerChosen() {
+    const playerOne = this.getFormField('playerOne')?.value;
+    const playerTwo = this.getFormField('playerTwo')?.value;
+    const guestOne = this.getFormField('guestOne')?.value;
+    const guestTwo = this.getFormField('guestTwo')?.value;
+    if (
+      (playerOne && this.isListOne) ||
+      (guestOne && !this.isListOne) ||
+      (playerTwo && this.isListTwo) ||
+      (guestTwo && !this.isListTwo)
+    ) {
+      this.isPlayerChosen = true;
+    } else {
+      this.isPlayerChosen = false;
+    }
+  }
+
+  submitForm() {
+    console.log(this.form.value);
   }
 
 }
